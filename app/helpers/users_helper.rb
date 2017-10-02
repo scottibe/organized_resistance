84799a61cccf4df51f4829e3b6756a3d4a408c6b
Scott Bewick <bewick.scott@gmail.com>
2017-10-02 16:18:02 -0500

    tweaked the user_helper method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
   def display_user(statement)
-    statement.user.nil? ? link_to("Add Author", edit_statement_path(statement)) : link_to(statement.user_name, user_path(statement.user))
+    statement.user.nil? ? 'Anonymous' : link_to(statement.user_name, user_path(statement.user))
   end
 
 end
